6187a4c974772417b7f0fdc9ddc6cd9e2cf59408
Anjali Mahapatra <mahapatraanjali24@gmail.com>
2026-08-16 14:03:24 +0530

    Implement basic calculator functionality in script.js

---
+++ TASK-3-calculator/script.js
@@ -0,0 +1,34 @@
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll("button");
+
+// Loop through all buttons
+for (let i = 0; i < buttons.length; i++) {
+
+    buttons[i].addEventListener("click", function () {
+
+        let value = this.innerText;
+
+        // If-else statements
+        if (value === "C") {
+            display.value = "";
+        }
+        else if (value === "<") {
+            display.value = display.value.slice(0, -1);
+        }
+        else if (value === "=") {
+
+            try {
+                display.value = eval(display.value);
+            }
+            catch {
+                display.value = "Error";
+            }
+
+        }
+        else {
+            display.value += value;
+        }
+
+    });
+
+}
